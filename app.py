945cc40cfb9e2bf6612abb456fb823cf929dd88c
MrAchraf10 <76006274+MrAchraf10@users.noreply.github.com>
2023-06-19 09:07:39 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,4 +17,5 @@ def predict():
     return jsonify(message)
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug = True)
+    from waitress import serve
+    serve(app, host="127.0.0.1", port=5000, debug = True)
